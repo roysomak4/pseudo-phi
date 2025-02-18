f6aac39bc3d3d15364290f15a209dbc5f03dfd3a
Somak Roy <roys@Somaks-Mac-mini.local>
2025-02-17 21:48:48 -0500

    Modified test script to output tab separated column data to STDOUT

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 from generator import generate_phi as gp
 
-print(gp(num_patients=4))
+pdata = gp(num_patients=20)
+
+for item in pdata:
+    print(
+        f"{item['firstname']}\t{item['lastname']}\t{item['minitial']}\t{item['dob']}\t{item['mrn']}\t{item['gender']}"
+    )
